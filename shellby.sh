ffa86a027a338cb16866498d04e691b17462a8c1
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-01 22:14:37 -0600

    hard mode

--- shellby.sh
+++ shellby.sh
@@ -6,3 +6,9 @@ mkdir texts
 mv *.txt texts
 cd texts
 rm deleteme.txt
+mkdir news
+cd news
+touch times.txt
+curl https://www.nytimes.com/ > times.txt
+touch national.txt
+grep America times.txt > national.txt
